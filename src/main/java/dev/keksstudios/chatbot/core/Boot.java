2d11d443581f8c4795ed535e163f011664f9f277
Maurice-Pascal L <larivieremauricepascal112@gmail.com>
2021-07-21 22:15:45 +0200

    First commit

---
+++ src/main/java/dev/keksstudios/chatbot/core/Boot.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2021 by MauricePascal
+ * Licensed under the GNU General Public License v3.0(the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+package dev.keksstudios.chatbot.core;
+
+/**
+ * The file where every important setting will be set up
+ * @author Maurice-Pascal L.
+ * @version ALPHA_1.0.0
+ */
+public class Boot {
+
+    /**
+     * The main function of the program
+     * @param args arguments from the command line
+     */
+    public static void main(String[] args) {
+    }
+
+}
